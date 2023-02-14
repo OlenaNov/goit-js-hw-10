9c38d1bff1feff2f5624a660a18653608f7b96bd
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-02-14 13:59:24 +0100

    left(style)

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -4,25 +4,56 @@ export default function fetchCountries(name) {
     const BASE_URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
     fetch(BASE_URL)
     .then(data => {
+
             if(!data.ok) {
-            countrysList.innerHTML = '';
-            throw new Error(Notify.failure('Oops, there is no country with that name'));
+            resertCountrysList();
+            Notify.failure('Oops, there is no country with that name');
+            throw new Error('Invalid country name');
         };
-        
+
         return data.json();
     })
     .then(contryDatas => {
+
         if(contryDatas.length > 10) {
+            resertCountrysList();
             Notify.info('Too many matches found. Please enter a more specific name.');
             return;
         };
 
-
         makeMarkupCountries(contryDatas);
-console.log(contryDatas);
+
         if(contryDatas.length === 1) {
             makeMarkupCountry(contryDatas);
         };
 })
     .catch(err => console.log(err));
+};
+
+
+const resertCountrysList = () => refs.countrysList.innerHTML = '';
+const addMarkup = (markup) => refs.countrysList.innerHTML = markup.join('');
+
+
+function makeMarkupCountries(arr) {
+    const markupCountrys = arr.map(({ name: { official }, flags: { svg } } ) =>
+        `<li>
+        <img src="${svg}" alt="${official}" width="30px">
+        <h2>${official}</h2>
+            </li>`);
+
+            addMarkup(markupCountrys);
+};
+
+function makeMarkupCountry(arr) {
+    const markupCountry = arr.map(({ name: { official }, capital, languages, population, flags: { svg } } ) =>
+        `<li>
+        <img src="${svg}" alt="${official}" width="30px">
+        <h2>${official}</h2>
+        <p>Capital: <span>${capital}</span></p>
+        <p>Population: <span>${population}</span></p>
+        <p>Languages: <span>${Object.values(languages)}</span></p>
+            </li>`);
+
+            addMarkup(markupCountry);
 };
--- src/index.js
+++ src/index.js
@@ -1,73 +1,27 @@
+
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
 import debounce from 'lodash.debounce';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import './fetchCountries';
 
-const inputSearchBox = document.querySelector('#search-box');
-const countrysList = document.querySelector('.country-list');
+refs = {  
+    inputSearchBox: document.querySelector('#search-box'),
+    countrysList: document.querySelector('.country-list'),
+};
 
-const DEBOUNCE_DELAY = 1000;
+const DEBOUNCE_DELAY = 300;
 
-inputSearchBox.addEventListener('input', debounce(onInputSearchBox, DEBOUNCE_DELAY));
+refs.inputSearchBox.addEventListener('input', debounce(onInputSearchBox, DEBOUNCE_DELAY));
 
 function onInputSearchBox(e) {
     let nameCountryInput = e.target.value.trim();
-    console.log(nameCountryInput);
+
     if(!nameCountryInput) {
-        countrysList.innerHTML = '';
+        resertCountrysList();
         return;
     };
+
     fetchCountries(nameCountryInput);
 };
 
-function fetchCountries(name) {
-    const BASE_URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-    fetch(BASE_URL)
-    .then(data => {
-            if(!data.ok) {
-            countrysList.innerHTML = '';
-            throw new Error(Notify.failure('Oops, there is no country with that name'));
-        };
 
 
-        return data.json();
-    })
-    .then(contryDatas => {
-        if(contryDatas.length > 10) {
-            Notify.info('Too many matches found. Please enter a more specific name.');
-            return;
-        };
-
-
-        makeMarkupCountries(contryDatas);
-console.log(contryDatas);
-        if(contryDatas.length === 1) {
-            makeMarkupCountry(contryDatas);
-        };
-})
-    .catch(err => console.log(err));
-};
-
-function makeMarkupCountries(arr) {
-    const markupCountrys = arr.map(({ name: { official }, flags: { svg } } ) =>
-        `<li>
-        <img src="${svg}" alt="${official}" width="30px">
-        <h2>${official}</h2>
-            </li>`);
-
-        countrysList.innerHTML = markupCountrys.join('');
-};
-
-
-function makeMarkupCountry(arr) {
-    const markupCountry = arr.map(({ name: { official }, capital, languages, population, flags: { svg } } ) =>
-        `<li>
-        <img src="${svg}" alt="${official}" width="30px">
-        <h2>${official}</h2>
-        <p>Capital: <span>${capital}</span></p>
-        <p>Population: <span>${population}</span></p>
-        <p>Languages: <span>${Object.values(languages)}</span></p>
-            </li>`);
-
-        countrysList.innerHTML = markupCountry.join('');
-};
